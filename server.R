c42d089619c48546f4eb8e376c860f08d3edead6
dkilfoyle <dkilfoyle@xtra.co.nz>
2016-05-21 10:27:42 +1200

    initial version

--- server.R
+++ server.R
@@ -1,23 +1,192 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
-shinyServer(function(input, output) {
+msevents = read.csv("events.csv", stringsAsFactors = F)
+msevents$DueDate = ymd(msevents$DueDate)
+msevents$Completed = ymd(msevents$Completed)
 
-  output$distPlot <- renderPlot({
+mspts = read.csv("patients.csv", stringsAsFactors = F)
+mspts$DateStarted = ymd(mspts$DateStarted)
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+server <- shinyServer(function(input, output, session) {
+  values = reactiveValues(msevents = msevents, mspts = mspts)
+  
+  # detect event selection
+  observe({
+    x = input$tlSelectEvent
+    if (is.null(x))
+      return()
+    cat(str(x))
+    if (x$id == "evtsTimeline") {
+      updateCollapse(session,
+        "evtsCollapse",
+        open = "Selected Event",
+        close = "Filter Events")
+      se = values$msevents[values$msevents$EventId == x$items$id,]
+      updateTextInput(session, "evtsId", value = x$items$id)
+      updateTextInput(session, "evtsStartDate", value = se$DueDate)
+      updateTextInput(session, "evtsType", value = se$Type)
+      updateTextInput(session, "evtsNumber", value = se$Number)
+      updateTextInput(session, "evtsResult", value = se$Result)
+      updateTextInput(session, "evtsComment", value = se$Comment)
+      updateTextInput(session, "evtsCompleted", value = se$Completed)
+      output$evtsInfo = renderUI(tagList(h3(x$items$NHI),p(values$mspts$Surname[values$mspts$NHI==x$items$NHI])))
+    }
   })
-
+  
+  observe({
+    x = input$tlMoveEvent
+    updateDateInput(session, "evtsStartDate", value = x$item$start)
+  })
+  
+  observeEvent(input$evtsCompletedButton, {
+    updateDateInput(session, "evtsCompleted", value = today())
+  })
+  
+  observeEvent(input$evtsSaveChanges, {
+    saveRow = which(values$msevents$EventId == input$evtsId)
+    values$msevents[saveRow, "DueDate"] = input$evtsStartDate
+    values$msevents[saveRow, "Type"] = input$evtsType
+    values$msevents[saveRow, "Number"] = input$evtsNumber
+    values$msevents[saveRow, "Result"] = input$evtsResult
+    values$msevents[saveRow, "Completed"] = input$evtsCompleted
+    values$msevents[saveRow, "Comment"] = input$evtsComment
+  })
+  
+  getFilteredEvents = reactive({
+    items = values$msevents
+    
+    nhi = toupper(input$evtsSearchNHI)
+    if (str_length(nhi) == 7) {
+      items = items %>%
+        filter(NHI == nhi)
+    }
+    
+    endDate = switch(
+      input$evtsTimeframe,
+      "All Pending" = ymd("2100/01/01"),
+      "This week" = today() + weeks(1),
+      "Next 6 weeks" = today() + weeks(6),
+      "Next 3 months" = today() + months(3),
+      ymd("2100/01/01")
+    )
+    endDate = as_date(endDate)
+    
+    if (input$evtsTimeframe == "Overdue") {
+      items = items %>%
+        filter(DueDate < today(), is.na(Completed))
+    } else if (input$evtsTimeframe == "Completed") {
+      items = items %>%
+        filter(!is.na(Completed))
+    } else
+    {
+      items = items %>%
+        filter(DueDate > today(), DueDate < endDate, is.na(Completed))
+    }
+    
+    items = items %>%
+      mutate(
+        content = Type,
+        start = DueDate,
+        group = NHI,
+        id = EventId
+      )
+    
+    items
+  })
+  
+  
+  output$evtsTimeline <- renderTimelinevis({
+    items = getFilteredEvents()
+    
+    if (nrow(items)==0) {
+      output$evtsFilterMessage = renderUI(h3("No Events Found"))
+      return(NULL)
+    }
+    else
+      output$evtsFilterMessage = renderUI("")
+    
+    groups = data.frame(
+      id = unique(items$NHI),
+      content = unique(items$NHI),
+      title = values$mspts$Surname[values$mspts$NHI == unique(items$NHI)]
+    )
+    timelinevis(
+      items,
+      groups,
+      id = "evtsTimeline",
+      editable = list(
+        add = F,
+        updateGroup = F,
+        updateTime = T,
+        remove = F
+      ),
+      snap = NULL
+    )
+  })
+  
+  output$evtsTable <- renderRHandsontable({
+    items = getFilteredEvents()
+    rhandsontable(values$msevents[values$msevents$EventId == items$id,])
+  })
+  
+  observeEvent(input$addpt, {
+    newrow = data.frame(
+      EventId = max(values$msevents$EventId) + 1,
+      NHI = "ACJ2321",
+      Type = "MRI",
+      Number = 1,
+      DueDate = dmy("1/23/2016"),
+      Completed = F
+    )
+    values$msevents = rbind(values$msevents, newrow)
+  })
+  
+  output$ptsTable = DT::renderDataTable(
+    values$mspts,
+    options = list(
+      lengthChange = F,
+      order = list(list(1, "asc")),
+      paging = F,
+      info = F
+    ),
+    selection = "single",
+    class = 'cell-border stripe'
+  )
+  
+  observe({
+    if (length(input$ptsTable_rows_selected) > 0) {
+      selrow = values$mspts[input$ptsTable_rows_selected,]
+      updateTextInput(session, "ptsNHI", value = selrow$NHI)
+      updateTextInput(session, "ptsFirstName", value = selrow$FirstName)
+      updateTextInput(session, "ptsSurname", value = selrow$Surname)
+      updateSelectInput(session, "ptsDrug", selected = selrow$Drug)
+      updateDateInput(session, "ptsDateStarted", value = selrow$DateStarted)
+    }
+  })
+  
+  observeEvent(input$ptsNew, {
+    updateTextInput(session, "ptsNHI", value = "Enter details")
+    updateTextInput(session, "ptsFirstName", value = "then")
+    updateTextInput(session, "ptsSurname", value = "click save")
+    updateSelectInput(session, "ptsDrug", selected = "")
+    updateDateInput(session, "ptsDateStarted", value = "")
+  })
+  
+  observeEvent(input$ptsSave, {
+    saveRow = which(values$mspts$NHI == input$ptsNHI)
+    newRow = list(
+      NHI = input$ptsNHI,
+      Surname = input$ptsSurname,
+      FirstName = input$ptsFirstName,
+      Drug = input$ptsDrug,
+      DateStarted = input$ptsDateStarted
+    )
+    
+    if (length(saveRow) == 0)
+      # new entry
+      values$mspts = rbind(values$mspts, newRow)
+    else
+      values$mspts[saveRow,] = newRow
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,148 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(rhandsontable)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(shinyBS)
+library(timelinevis)
 
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+textareaInput <-
+  function(id,
+    label,
+    value = "",
+    rows = 5,
+    cols = 20,
+    class = "form-control") {
+    tags$div(
+      class = "form-group shiny-input-container",
+      tags$label('for' = id, label),
+      tags$textarea(
+        id = id,
+        class = class,
+        rows = rows,
+        cols = cols,
+        value
+      )
     )
+  }
+
+hiddenTextInput = function (inputId,
+  label,
+  value = "",
+  width = NULL,
+  placeholder = NULL)
+{
+  tags$input(
+    id = inputId,
+    type = "text",
+    class = "form-control",
+    value = value,
+    placeholder = placeholder,
+    style = "display:none;"
   )
+}
+
+headerPanel = function (title, ...) {
+  div(class="header-panel", div(class="panel panel-default",
+    div(class="panel-heading",
+      h3(title, class="panel-title")
+    ),
+    div(class="panel-body", ...)
+  ))
+}
+
+ui <- shinyUI(navbarPage(
+  "MS Monitoring Program",
+  
+  tabPanel("Events",
+    sidebarLayout(
+      sidebarPanel(
+        includeCSS("www/msmonitor.css"),
+        headerPanel("Filter Events",
+          textInput("evtsSearchNHI", "NHI", placeholder = "Leave blank to search all"),
+          radioButtons(
+            "evtsTimeframe",
+            "Timeframe",
+            choices = c(
+              "All Pending",
+              "This week",
+              "Next 6 weeks",
+              "Next 3 months",
+              "Overdue",
+              "Completed"
+            )
+          )
+        )
+      ), # end sidebarPanel
+      mainPanel(tabsetPanel(
+        tabPanel(
+          "Timeline",
+          uiOutput("evtsFilterMessage"),
+          timelinevisOutput("evtsTimeline"),
+          bsCollapse(
+            bsCollapsePanel(
+              "Selected Event",
+              fluidRow(
+                column(width=4,
+                  hiddenTextInput("evtsId", "Id"),
+                  selectInput("evtsType", "Type", choices=c("LFT","MRI","JCV","FBC"), selected=NULL),
+                  textInput("evtsNumber", "Number", ""),
+                  textareaInput("evtsComment", "Comment")
+                ),
+                column(width=4,
+                  dateInput("evtsStartDate", "Due Date", ""),
+                  dateInput("evtsCompleted", "Date Completed", ""),
+                  textareaInput("evtsResult", "Result")
+                ),
+                column(width=4,
+                  headerPanel("Patient Info",
+                    uiOutput("evtsInfo")
+                  ),
+                  div(
+                    class = "btn-group-vertical",
+                    role = "group",
+                    style = "width:100%",
+                    actionButton("evtsCompletedButton", "Mark as Completed"),
+                    actionButton("evtsGenerateNew", "Generate new event"),
+                    actionButton("evtsSaveChanges", "Save Changes")
+                  )
+                ))
+            ),
+            id = "evtsCollapse",
+            multiple = T
+          )
+        ), # end timeline tabPanel
+        tabPanel("Table", rHandsontableOutput("evtsTable"))
+      )) # end mainpanel
+    )), # end tabpanel Events
+  tabPanel(
+    "Patients",
+    DT::dataTableOutput("ptsTable"),
+    tags$hr(),
+    
+    fluidRow(
+      column(
+        width = 6,
+        textInput("ptsNHI", "NHI", ""),
+        textInput("ptsSurname", "Surname", ""),
+        textInput("ptsFirstName", "First Name", "")
+      ),
+      column(
+        width = 6,
+        selectInput(
+          "ptsDrug",
+          "Drug",
+          choices = c("Tecfidera", "Natalizumab", "Fingolimod", "Interferon"),
+          selected = ""
+        ),
+        dateInput("ptsDateStarted", "Date Started", "")
+      )
+    ),
+    
+    #action buttons
+    actionButton("ptsSave", "Save"),
+    actionButton("ptsNew", "New"),
+    actionButton("ptsDelete", "Delete")
+  ), # end tabpanel patients
+  tabPanel("Setup")
 ))
